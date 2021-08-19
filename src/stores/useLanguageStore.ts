a5da812e85abc5947a57c7d2c6676c8645ebeed9
mmarganski <mikolaj.marganski@gmail.com>
2021-08-19 14:03:37 +0200

    added saving selected language in localStorage

--- src/stores/useLanguageStore.ts
+++ src/stores/useLanguageStore.ts
@@ -1,8 +1,17 @@
-import { useState } from 'react'
-import { LanguageNames, LanguageDictionaries } from '../languages'
+import { useEffect, useState } from 'react'
+import { LanguageDictionaries, LanguageNames } from '../languages'
 
 export const useLanguageStore = () => {
-    const [currentLanguage, setLanguage] = useState<LanguageNames>(LanguageNames.eng)
+
+    const [currentLanguage, setLanguage] = useState<LanguageNames>(
+        window.localStorage.getItem('language')
+            ? JSON.parse(window.localStorage.getItem('language') as string)
+            : LanguageNames.eng
+    )
+
+    useEffect(() => {
+        window.localStorage.setItem('language', JSON.stringify(currentLanguage))
+    }, [currentLanguage])
 
     return {
         currentLanguage,
